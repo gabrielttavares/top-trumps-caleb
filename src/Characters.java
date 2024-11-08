b3e82fe288726a925f3e262c993876ad0e55522f
unknown <gabrieltxtavares@gmail.com>
2024-11-07 22:10:52 -0300

    Start to create characters objects.

---
+++ src/Characters.java
@@ -0,0 +1,10 @@
+public class Characters extends Abilities{
+    public Characters(String name, int strength, int wisdom, double faithLevel, float humility, String specialAbility, String tribe, boolean isLeader) {
+        super(name, strength, wisdom, faithLevel, humility, specialAbility, tribe, isLeader);
+    }
+
+    public static void main(String[] args) {
+        Abilities mosesAbilities = new Abilities("Moses", 70, 70, 30, 2, "Parting the Sea", "Levi", true);
+        mosesAbilities.toString();
+    }
+}
