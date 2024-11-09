6ee2c28267811485a5d2f85691ac01a419076d9a
unknown <gabrieltxtavares@gmail.com>
2024-11-09 20:25:50 -0300

    Add Database Connection

--- src/Characters.java
+++ src/Characters.java
@@ -1,29 +1,54 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Characters{
-    private List<Abilities> characters;
+public class Characters {
+    private List<Character> characters;
 
     public Characters() {
         this.characters = new ArrayList<>();
     }
 
-    public void addCharacter(Abilities abilities){
-        characters.add(abilities);
-    }
+    public void loadCharacterFromDatabase(Connection connection) {
+        String query = "SELECT * FROM characters";
 
-    public void findCharacterByName(String name) {
-        for (Abilities character : characters) {
-            if (character.getName().equalsIgnoreCase(name)) {
-                System.out.println(character);
-                return;
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query)) {
+
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                int strength = resultSet.getInt("strength");
+                int wisdom = resultSet.getInt("wisdom");
+                double faithLevel = resultSet.getDouble("faith_level");
+                float humility = resultSet.getFloat("humility");
+                String specialAbility = resultSet.getString("special_ability");
+                String tribe = resultSet.getString("tribe");
+                boolean isLeader = resultSet.getBoolean("is_leader");
+
+                Character character = new Character(name, strength, wisdom, faithLevel, humility, specialAbility, tribe, isLeader);
+                characters.add(character);
             }
+            System.out.println("Characters loaded from the database!");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        System.out.println("Character not found.");
     }
 
-    public static void main(String[] args) {
-        Abilities mosesAbilities = new Abilities("Moses", 70, 70, 30, 4, "Parting the Sea", "Levi", true);
-        System.out.println(mosesAbilities);
+    public void displayAllCharacters() {
+        for (Character character : characters) {
+            System.out.println(character);
+        }
     }
 }
+
+//    public void findCharacterByName(String name) {
+//        for (Character character : characters) {
+//            if (character.getName().equalsIgnoreCase(name)) {
+//                System.out.println(character);
+//                return;
+//            }
+//        }
+//        System.out.println("Character not found.");
+//    }
