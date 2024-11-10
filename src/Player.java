f00c39276154dd1056603614a0f481d027b05d31
unknown <gabrieltxtavares@gmail.com>
2024-11-10 13:24:00 -0300

    Add Player.java logic: lists and methods for add cards, play card, win cards

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
+	private String name;
+	private List<Character> hand;
+	
+	public Player(String name) {
+		this.name = name;
+		this.hand = new ArrayList<Character>();
+	}
+	
+	public void addCards(List<Character> cards) {
+		hand.addAll(cards); // Add to bottom of the hand
+	}
+	
+	public Character playCard() {
+		if (hand.isEmpty()) {
+			return null;
+		}
+		return hand.remove(0); // Removes and returns top card
+	}
+	
+	public void winCards(List<Character> wonCards) {
+		hand.addAll(wonCards); // To the bottom
+	}
+	
+	public boolean hasCards() {
+		return !hand.isEmpty();
+	}
+	
+	public String getName() {
+		return name;
+	}
 }
