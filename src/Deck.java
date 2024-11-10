b48a1a8a1b08e8ea3cc8cdc6235e78f07b8c3492
unknown <gabrieltxtavares@gmail.com>
2024-11-10 13:09:46 -0300

    Add Deck.java logic: holds cards, method to shuffle cards and manage dealing cards to players

--- src/Deck.java
+++ src/Deck.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Deck {
+	private List<Character> cards;
+	
+	public Deck(List<Character>characters) {
+		this.cards = new ArrayList<Character>(characters);
+	}
+	
+	public void shuffle() {
+		Collections.shuffle(cards);
+	}
+	
+	public List<Character> deal(int numberOfCards) {
+		List<Character> hand = new ArrayList<>(cards.subList(0, numberOfCards));
+		cards.subList(0, numberOfCards).clear();
+		return hand;
+	}
+	
+	public boolean isEmpty() {
+		return cards.isEmpty();
+	}
 }
